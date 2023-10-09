7145ea40dcd642ad54d729518fa313b3354dee78
gheorghe.mirca <gheorghemirca2000@gmail.com>
2023-10-09 21:57:58 +0300

    added credentials for user pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_EHjFaucTE', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1kgi882sn6uc3s4t4g4trp75g2', // e.g. eu-north-1_EHjFaucTE
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
